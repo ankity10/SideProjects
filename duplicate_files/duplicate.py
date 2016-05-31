0489c7f347be8b8817ea4a72c34d1a003defa1e3
Rushabh Kabra <meetrushabh@gmail.com>
2016-05-31 20:13:35 +0530

    Getting sha1 value

--- duplicate_files/duplicate.py
+++ duplicate_files/duplicate.py
@@ -1,15 +1,30 @@
 import os
+import hashlib
 from collections import defaultdict
 
-os.system("find -not -empty -type f -printf '%s\t%i\n'>a.txt")
+def hashfile(file_path):
+    sha1 = hashlib.sha1()
+    f = open(file_path, 'rb')
+    try:
+        sha1.update(f.read())
+    finally:
+        f.close()
+    return sha1.hexdigest()
+
+os.system("find -not -empty -type f -printf '%s\t%p\n'>a.txt")
 fil=open("a.txt","r");
 n=0
 a=defaultdict(list)
 for line in fil:
 	size=int
-	inode=int
-	size,inode=line.split()	
-	a[size].append(inode)	
-print a.items()
+	path=str
+	size,path=line.split()	
+	a[size].append(path)
 
+sha1_list = defaultdict(list)
 
+for size, path in a.items():
+	for file_path in path:
+		sha1_list[hashfile(file_path)].append(file_path)
+
+print sha1_list.items()

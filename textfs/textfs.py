06775feab1c6a5ff79a3439a7f2fdf041752af76
nisha khiyani <nishakhiyani123@gmail.com>
2016-06-01 20:39:59 +0530

    adding validation

--- textfs/textfs.py
+++ textfs/textfs.py
@@ -24,24 +24,28 @@ class file_class(object):
 
 
 #f.create("b.txt")
-io=shelve.open("textfs.sys",writeback=True)
-print "COMMANDS:create,delete,echo,copy"
+io=shelve.open(".textfs.sys",writeback=True)
+COMMANDS=["create","copy","delete","echo","exit","ls"]
+print "COMMANDS :create,copy,ls,delete,echo,exit,help"
 cmd=raw_input(">").split()
 
-while(cmd[0]!="exit"):
+while(True):
 
 	if cmd[0]=="create":
-		found=0
-		for inode_no, obj in io.items():
-			if obj.name==cmd[1]:
-				found=1
-				break
-		if found:
-			print "File "+cmd[1]+" already exists"
+		if len(cmd)!=2:
+			print "create----usage:\ncreate filename"
 		else:
-			f=file_class()
-			f.create(cmd[1])
-			io[f.inode_no]=f
+			found=0
+			for inode_no, obj in io.items():
+				if obj.name==cmd[1]:
+					found=1
+					break
+			if found:
+				print "File "+cmd[1]+" already exists"
+			else:
+				f=file_class()
+				f.create(cmd[1])
+				io[f.inode_no]=f
 
 	elif cmd[0]=="copy":
 		if len(cmd)==3:
@@ -57,27 +61,42 @@ while(cmd[0]!="exit"):
 				f.copy(cmd[1],cmd[2])
 				io[f.inode_no]=f
 				print len(io.items())
-
+		else:
+			print "copy-------usage:\ncopy source destination"
 	elif cmd[0]=="echo":
-		found=0
-		for inode_no,obj in io.items():
-			if obj.name==cmd[1] :
-				obj.echo()
-				found=1
-		if found==0:
-			print "File not found"
+		if len(cmd)!=2:
+			print "echo-------usage:\necho filename"
+		else:
+			found=0
+			for inode_no,obj in io.items():
+				if obj.name==cmd[1] :
+					obj.echo()
+					found=1
+			if found==0:
+				print "File not found"
 
 	elif cmd[0]=="delete":
-		found=0
-		for inode_no,obj in io.items():
-			if obj.name==cmd[1]:
-				del io[obj.inode_no]
+		if len(cmd)!=2:
+			print "delete-------usage:\ndelete filename"
+		else:
+			found=0
+			for inode_no,obj in io.items():
+				if obj.name==cmd[1]:
+					del io[obj.inode_no]
 
 	elif cmd[0]=="ls":
-		for inode_no,obj in io.items():
-			print obj.name
-
-
+		if len(cmd)!=1:
+			print "ls------usage:ls"
+		else:
+			for inode_no,obj in io.items():
+				print obj.name
+	elif cmd[0]=="help":
+		print COMMANDS
+	elif cmd[0]=="exit":
+		if len(cmd)!=1:
+			print "exit----usage:exit"
+		else:
+			sys.exit(0)
 	else:
 		print "Invalid Command"
 

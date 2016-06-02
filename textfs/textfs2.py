1dbe8ed3a2b81ab73c48dbea2adaa79e8da00aa6
nisha khiyani <nishakhiyani123@gmail.com>
2016-06-02 16:37:58 +0530

    added delete functionality in textfs2.py

--- textfs/textfs2.py
+++ textfs/textfs2.py
@@ -7,16 +7,16 @@ def create(file_name):
 			sb_obj.write(file_name+";"+pos+";"+pos+";"+str(time.time())+"\n")
 
 
-def copy(file_name,destination):
+def copy(source,destination):
 	with open("superblock.txt","a+") as sb_obj:
 		with open("textfs.txt","a") as textfs_obj:
-			with open(destination,"r") as des:
+			with open(source,"r") as des:
 				pos=str(textfs_obj.tell())
 				inode_no=str(time.time())
-				textfs_obj.write(inode_no+";"+file_name+";"+des.read())
+				textfs_obj.write(inode_no+";"+destination+";"+des.read())
 				#textfs_obj.write(des.read())
 				pos2=str(textfs_obj.tell())
-				sb_obj.write(file_name+";"+pos+";"+pos2+";"+str(time.time())+"\n")
+				sb_obj.write(destination+";"+pos+";"+pos2+";"+str(time.time())+"\n")
 
 
 def echo(file_name):
@@ -40,49 +40,40 @@ def ls():
 
 
 def delete(file_name):
+	sb_upper=""
+	sb_lower=""
+	textfs_upper=""
+	textfs_lower=""
 	with open("superblock.txt","r") as sb_obj:
 		with open("textfs.txt","r") as textfs_obj:
-			with open("superblock_temp.txt","a+") as sb_temp_obj:
-				with open("textfs_temp.txt","a+") as textfs_temp_obj:
-					f_start=int
-					f_end=int
-					found=0
-					for sb_info in sb_obj:
-						sb_file_info=sb_info.split(";")
-						if sb_file_info[0]==file_name:
-							print "File found"
-							f_start=int(sb_file_info[1])
-							f_end=int(sb_file_info[2])
-							found=1
-						else:
-							if found==1:
-								sb_file_info[1]=int(sb_file_info[1])-f_start
-								sb_file_info[2]=int(sb_file_info[2])-f_end
-							sb_temp_obj.write(sb_file_info)
-
-					textfs_temp_obj.write(textfs_obj.read(f_start))
-					textfs_obj.seek(f_end,0)
-					textfs_temp_obj.write(textfs_obj.read())
-	os.system("rm superblock.txt")
-	os.system("rm textfs.txt")
-	os.system("mv superblock_temp.txt superblock.txt")
-	os.system("mv textfs_temp.txt textfs.txt")
-
-
+			found=0
+			f_start=int
+			f_end=int
+			for sb_info in sb_obj:
+				sb_file_info=sb_info.split(";")
+				if sb_file_info[0]==file_name:
+					f_start=int(sb_file_info[1])
+					f_end=int(sb_file_info[2])
+					print "File found"
+					found=1
+				else:
+					if not found:
+						sb_upper=sb_upper+str(sb_info)
+					else:
+						sb_file_info[1]=int(sb_file_info[1])-(f_end-f_start)
+						sb_file_info[2]=int(sb_file_info[2])-(f_end-f_start)
+						temp=str(sb_file_info[0])+";"+str(sb_file_info[1])+";"+str(sb_file_info[2])+";"+str(sb_file_info[3])
+						sb_lower=sb_lower+str(temp)
+			textfs_upper=textfs_obj.read(f_start)
+			textfs_obj.seek(f_end,0)
+			textfs_lower=textfs_obj.read()
+	with open("superblock.txt","w") as sb_obj:
+		with open("textfs.txt","w") as textfs_obj:
+			sb_obj.write(sb_upper+sb_lower)
+			textfs_obj.write(textfs_upper+textfs_lower)
 
 
-
-
-
-
-
-
-
-
-
-
-
-print "COMMANDS:create,delete,echo,copy"
+print "COMMANDS:create,delete,echo,copy,exit,ls"
 cmd=raw_input(">").split()
 
 while(cmd[0]!="exit"):
@@ -97,3 +88,4 @@ while(cmd[0]!="exit"):
 	elif cmd[0]=="delete":
 		delete(cmd[1])
 	cmd=raw_input(">").split()
+
